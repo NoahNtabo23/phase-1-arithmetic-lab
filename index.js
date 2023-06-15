dfc50e798cff19de1ebee0979bb6499669a457b3
NoahNtabo23 <135032672+NoahNtabo23@users.noreply.github.com>
2023-06-15 23:21:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// function multiply(num1,num2){
+//    return (num1*num2)==62
+// }
+// console.log(num1*num2==62)
+let num1=31;
+let num2=2;
+let multiply=num1*num2
 
-// Write your code here
+const random=Math.floor(Math.random()*10)+1
+
+let num3=64;
+let num4=10;
+let mod=num3%num4
+
+const max=Math.max(20,12,10,8,5,4,2,1)
+
